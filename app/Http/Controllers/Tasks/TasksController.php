73351999c801eda9259c444cfac3c7971bffe800
Someone <rgbjkbs@hdbf.ru>
2020-12-18 20:20:27 +0200

    Controller add action, auth middleware

--- app/Http/Controllers/Tasks/TasksController.php
+++ app/Http/Controllers/Tasks/TasksController.php
@@ -1,19 +1,43 @@
 <?php
-
-namespace App\Http\Controllers\Tasks;
-
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
-class TasksController extends Controller
-{
-    public function index() {
-        return view('tasks.index');
+    namespace App\Http\Controllers\Tasks;
+    use App\Models\Task;
+    use Illuminate\Http\Request;
+    use App\Http\Requests;
+    use App\Http\Controllers\Controller;
+    use Illuminate\Support\Facades\Auth;
+    class TasksController extends Controller
+    {
+        public function __construct()
+        {
+            $this->middleware('auth'); // посредник
+        }
+        public function index()
+        {
+            return view('tasks.index');
+        }
+        public function create()
+        {
+            return view('tasks.create');
+        }
+        public function add(Request $request)
+        {
+            $this->validate($request, ['name' => 'required|max:255']);
+            ###v1###############
+//            $user = Auth::user();
+//            dd($user);
+            ####################
+            ###v2###############
+            $user = $request->user();
+//            dd($user->tasks); // получение всех задач
+//            dd($user->tasks()); // получаем класс HasMany
+            $user->tasks()->create(
+                    ['name'=>$request->name,]
+            );
+            ####################
+//            $task = new Task();
+//            $task->name = $request->name;
+//            $task->user_id = $user->id;
+//            $task->save();
+            return redirect(route('tasks.index'));
+        }
     }
-    
-    public function create() {
-        return view('tasks.create');
-    }
-}
